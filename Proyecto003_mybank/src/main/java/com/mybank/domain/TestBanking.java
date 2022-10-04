31a0ebbe8404f974b4fcade67c613facc37cccfa
Conrado <cayuso@birt.eus>
2022-10-04 20:04:47 +0200

    Solución 2 de Práctica_mod4.pdf

--- Proyecto003_mybank/src/main/java/com/mybank/domain/TestBanking.java
+++ Proyecto003_mybank/src/main/java/com/mybank/domain/TestBanking.java
@@ -4,19 +4,24 @@ public class TestBanking {
 
 	public static void main(String[] args) {
 		
-		Account ac= new Account(20.00);
+		Account ac= new Account(500.00);
 		
-		//Customer cu=new Customer(null, null)
+		Customer cu=new Customer("Luis ","Ramos");
 		
-		System.out.println("Creando el cliente" );
+		System.out.println("Creando el cliente "+ cu.getFirstName()+ cu.getLastName());
 		
 		System.out.println("Creando su cuenta bancario con saldo "+ ac.getBalance() );
+		
+		System.out.println("Sacar "+ ac.withdraw(150.00));
 
-		//System.out.println("Ingresar "+ ac. );
+		System.out.println("Ingresar "+ ac.deposit(22.50));
 		
-		//System.out.println("Sacar "+ ac. );
+		System.out.println("Sacar "+ ac.withdraw(47.62) );
 		
-		System.out.println("Creando el cliente" );
+		System.out.println("Sacar "+ ac.withdraw(400.00) );
+		
+		System.out.println("Customer ["+ cu.getFirstName()+", "+ cu.getLastName()+"] tiene un saldo de "+ ac.getBalance() );
+		
 
 	}
 
