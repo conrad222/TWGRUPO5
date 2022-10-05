8379d7f9799150c98072adaad4f790aa1bae984c
conrad222 <76107590+conrad222@users.noreply.github.com>
2022-10-05 14:27:26 +0200

    sigo con la máquina

---
+++ Proyecto005MExpendedora/src/main/java/com/curso/pruebas/App.java
@@ -0,0 +1,10 @@
+package com.curso.pruebas;
+
+public class App {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
---
+++ Proyecto005MExpendedora/src/main/java/com/curso/pruebas/Refrescos.java
@@ -0,0 +1,28 @@
+package com.curso.pruebas;
+
+public class Refrescos {
+	
+	private String sabor;
+	private String cl;
+	private double precio;
+	private String agotado;
+	private int stock;
+	
+	
+	public Refrescos(String sabor, String cl, double precio, String agotado, int stock) {
+		super();
+		this.sabor = sabor;
+		this.cl = cl;
+		this.precio = precio;
+		this.agotado = agotado;
+		this.stock = stock;
+	}
+	
+	
+	public void reponer(Refrescos refresco) {
+		
+		
+	}
+	
+
+}
