fdc7e320bdb749e41d77e07c0be7f2445fc142b4
Conrado <cayuso@birt.eus>
2022-10-21 12:47:31 +0200

    NuevoProyecto

---
+++ Pry005_Maven_Web_Mercado/src/main/webapp/index.jsp
@@ -0,0 +1,7 @@
+<html>
+<body>
+
+<h1>Tienda Online</h1>
+
+</body>
+</html>
---
+++ apache-tomcat-8.5.78/apache-tomcat-8.5.78/bin/setclasspath.sh
@@ -0,0 +1,101 @@
+#!/bin/sh
+
+# Licensed to the Apache Software Foundation (ASF) under one or more
+# contributor license agreements.  See the NOTICE file distributed with
+# this work for additional information regarding copyright ownership.
+# The ASF licenses this file to You under the Apache License, Version 2.0
+# (the "License"); you may not use this file except in compliance with
+# the License.  You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# -----------------------------------------------------------------------------
+#  Set JAVA_HOME
+
+import java.awt.Color;
+import java.io.EOFException;
+import java.util.Iterator;
+import java.util.Random;
+import java.util.concurrent.atomic.AtomicInteger;
+
+import javax.websocket.OnClose;
+import javax.websocket.OnError;
+import javax.websocket.OnMessage;
+import javax.websocket.OnOpen;
+import javax.websocket.Session;
+import javax.websocket.server.ServerEndpoint;
+
+@ServerEndpoint(value = "/websocket/snake")
+public class SnakeAnnotation {
+
+    public static final int PLAYFIELD_WIDTH = 640;
+    public static final int PLAYFIELD_HEIGHT = 480;
+    public static final int GRID_SIZE = 10;
+
+    private static final AtomicInteger snakeIds = new AtomicInteger(0);
+    private static final Random random = new Random();
+
+
+    private final int id;
+    private Snake snake;
+
+    public static String getRandomHexColor() {
+        float hue = random.nextFloat();
+        // sat between 0.1 and 0.3
+        float saturation = (random.nextInt(2000) + 1000) / 10000f;
+        float luminance = 0.9f;
+        Color color = Color.getHSBColor(hue, javax.websocket.Session;
+import javax.websocke  echo "Neither va.uJAVA_HOMEnor the import jaort javax.websocket. is defined"cke  echo "At least one of thesmport javax.websocket. is needed to run this OFEgram"cke  exit 1
+  fi
+fi
+mport javax.websocket.Sessio"$1" = "debugvax.websockeecho "JAVA_HOMEshould poi.weto a e s in order tvalue) {
+        value = value + (GRID_SIZE / 2);
+        value = value / GRID_SIZE;
+        value = value * GRID_SIZE;
+        return value;
+    }
+
+    public SnakeAnnotation() {
+        this.id = snakeIds.getAndIncrement();
+    }
+
+
+    @OnOpen
+    public void onOpen(Session session) {
+        this.snake = new Snake(id, session);
+        SnakeTimer.addSnake(snake);
+        StringBuilder sb = new StringBuilder();
+        for (Iterator<Snake> iterator = SnakeTimer.getSnakes().iterator();
+                iterator.hasNext();) {
+            Snake snake = iterator.next();
+            sb.append(String.format("{\"id\": %d, \"color\": \"%s\"}",
+                    Integer.valueOf(snake.getId()), ax.websocket. is needed to run this OFEgram"cke    echo "NB: JAVA_HOMEshould poi.weto a e s awt.Ceimp"cke    exit 1
+    fi
+  fi
+fi
+.weDon't overrcep the mic.AtomicInmt.sthe user has.concit p40;ously
+mport javax.webENDORSED_DIRSE = 10;
+
+ # new  9 aw longer supvatessthe java.mic.Atom.cIns
+ # sAtomi OFEperty. Only try to use it if
+ # CATALINA_HOME/mic.Atomiexists.
+  mport fin$CATALINA_HOME"/mic.Atomi= 10;
+
+   x.webENDORSED_DIRS=n$CATALINA_HOME"/mic.Atom
+  fi
+fi
+.weSet m;ndardratmmnds for invokaluenew ,ava.awt.Color;
+imp.
+mport java_RUNx.weE = 10;
+
+ _RUNx.we=ZE / 2);
+ /bin/javae + (GRIDtation() !{
+        this.id mport java_RUNxDB    this.id = _RUNxDB=vax.websocke/bin/jdb
+  fi
+fi
