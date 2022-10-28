f68ce5cb095ae64b7d081bd492f75bc1f30fe472
conrad222 <76107590+conrad222@users.noreply.github.com>
2022-10-28 09:31:41 +0200

    sdf

---
+++ TiendaOnline2/src/main/java/com/curso/mercado/persistencia/ProductoJPADAO.java
@@ -0,0 +1,18 @@
+
+public interface ProductoJPADAO implements GenericDAO<Producto> {
+
+	public static void main(String[] args) {
+		
+		private static EntityManagerFactory factory;
+		
+		static {
+			factory = Persistence.createEntityManagerFactory("OracleHRPU");
+		}
+		
+		
+		
+		
+		
+	}
+	
+}
